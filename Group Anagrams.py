3070412ce3606f9998098c2165590ad8eda15cfd
Parshva Timbadia <parshwat@gmail.com>
2020-04-07 11:32:09 +0530

    Create Group Anagrams.py

---
+++ Group Anagrams.py
@@ -0,0 +1,35 @@
+***
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+***
+
+
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        result=[]
+        list={}
+        for word in strs:
+            Sortedword= "".join(sorted(word))
+            
+            if Sortedword not in list:
+                list[Sortedword]=[word]
+            else:
+                list[Sortedword].append(word)
+                
+                
+        for i in list:
+            result.append(list[i])
+            
+        return result    
